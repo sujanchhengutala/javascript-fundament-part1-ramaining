94b4d94b60a95edd0c0d5174aa3f9b1253cd791e
sujanchhengutala9@gmail.com <sujanchhengutala9@gmail.com>
2021-06-16 17:03:50 +0545

    type conversion & coercion

---
+++ 8-type conversion and coercion/script.js
@@ -0,0 +1,39 @@
+// type conversion
+// Guess the output
+// const inputYear = '1991';
+// console.log(inputYear + 18);
+
+// console.log(Number(inputYear) + 18);
+
+//inputYear is not yet converted
+// console.log(inputYear + 18);
+
+// console.log(Number("Achiever"));
+
+// console.log(typeof NaN);
+
+// console.log(String(23));
+// console.log(typeof String(23));
+
+// JS can only convert to Number, to String and to Boolean
+
+//Boolean conversion will be discussed later.
+
+// type coercion
+// console.log("I'm " + 23 + " years old");
+// template literals also converts to strings
+//No need to do
+// console.log("I'm " + String(23) + " years old");
+
+// console.log('90' - '10' - 5);
+// console.log('50' / '10');
+
+// console.log('20' > '10');
+
+// let n = '1' + 1;
+// n = n - 1;
+// console.log(n-1);
+
+// console.log(2 + 3 + 4 + '5');  
+// console.log('10' - '4' - '3' - '2' + '5')
+console.log('10' - '4' - '3' - '2' + 1)
